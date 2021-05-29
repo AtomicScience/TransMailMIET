8939aa9238f4fd915e5b4cd0e1713f7f442265b8
AtomicScience <dmitry.greblyuk@mail.ru>
2021-05-29 19:48:06 +0300

    Initial project commit

---
+++ src/test/java/ru/techain/TransMail/TransMailApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.techain.TransMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
