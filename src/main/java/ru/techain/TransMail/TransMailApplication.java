9d32cb040ec1a5ac546b264ef6029058192feae7
SanVerrigo <DooPfg120BeRorar>
2021-05-30 10:02:14 +0300

    Добавил сэмплы

--- src/main/java/ru/techain/TransMail/TransMailApplication.java
+++ src/main/java/ru/techain/TransMail/TransMailApplication.java
@@ -11,30 +11,49 @@ import org.springframework.util.unit.DataUnit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.techain.TransMail.api.FullLotTableParser;
+import ru.techain.TransMail.api.email.entities.Letter;
+import ru.techain.TransMail.api.email.entities.LetterBuilder;
+import ru.techain.TransMail.api.models.FinalEmployee;
+import ru.techain.TransMail.api.table.RealFullLotTableParser;
 import ru.techain.TransMail.web.controllers.FileUploadController;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
 public class TransMailApplication {
     public static void main(String[] args) {
 		SpringApplication.run(TransMailApplication.class, args);
-//        Letter letter = new LetterBuilder().addAttachment(new File("C:\\Users\\HASEE\\Desktop\\sample.pdf"))
-//                .addAttachment(new File("C:\\Users\\HASEE\\Desktop\\sample1.pdf"))
-//                .setContent("Test")
-//                .setFromEmail(EmailConsts.PODSATAVA007_YANDEX)
-//                .setPassword(EmailConsts.PODSATAVA007_YANDEX_PASSWORD)
-//                .setToEmail(EmailConsts.VERRIGOOFF_GMAIL)
-//                .setSubject("Test for attachments")
-//                .build();
-//        try {
-//            new EmailRepo().sendLetter(letter);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+//		File pdfDirectory = new File("src/main/resources/static/pdf");
+//
+//		FullLotTableParser parser = new RealFullLotTableParser();
+//		List<File> files = new ArrayList<>(Arrays.asList(Objects.requireNonNull(pdfDirectory.listFiles())));
+//
+//		List<FinalEmployee> employees = parser.getFinalEmployeesByProduct("Сберпрайм");
+//		int i = 0;
+//		for (FinalEmployee e: employees) {
+//			LetterBuilder lb = new LetterBuilder();
+//			String str = String.format("Уважаемый %s, вас ожидает ваш подарок: %s",
+//					e.getName() + " " + e.getPatronymic(), e.getNameProduct());
+//
+//			if (e.getNameProduct().equals("Онлайн-подписка Сберпрайм (12 месяцев)"))
+//
+//			lb.setFromEmail("ENTERYOUTEMAIL@gmail.com")
+//					.setPassword("ENTERYOURPASSWORD")
+//					.setSubject("Ваш подарок!")
+//					.setContent()
+//					.setToEmail(e.getEmail());
+//
+//
+//		}
+
+
     }
 
     @Bean
